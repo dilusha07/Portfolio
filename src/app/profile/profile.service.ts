b9e7928eb46bfeba0207f0d8d35211ddba434760
dilusha07 <dilushasandaruwani07@gmail.com>
2022-07-24 09:42:28 +0530

    Add profile service

---
+++ src/app/profile/profile.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+  constructor() {}
+
+  about2 = `Software Developer with 1 year of experience .
+  Worked with Software Development Life Cycle(SDLC) and  agile methodologies .
+  Expertise in Angular All versions, React, Javascript,  Node JS and UI with Responsive Designs .
+  Have good understanding of Python, Jquery, Bootstrap, MongoDB, aws, and MYSQL`;
+
+  about1 =
+    'Lover of innovation and everything related to generate new knowledge.Face problems with a smile and solve them as soon as possible. Very calculated about the time I spend and work I do.';
+}
